b4f59c1ec4bd03b089afcd007c75575fea4b24fc
jcleira <jmc.leira@gmail.com>
2019-07-20 07:58:28 +0200

    Add chop unit tests to test basic implementation
    This commit adds chop unit tests, they have been copied from the kata
    page. I would be using assert even when it's not totally necessary (and
    I'm adding a test dependency there). Also the test are implemented using
    table testing as It looks like the perfect solution for the multiple
    test cases.

---
+++ basic/basic_test.go
@@ -0,0 +1,43 @@
+package basic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChop(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		Target int
+		Array  []int
+		Result int
+	}{
+		{3, []int{}, -1},
+		{1, []int{1}, 0},
+		{3, []int{1}, -1},
+
+		{1, []int{1, 3, 5}, 0},
+		{3, []int{1, 3, 5}, 1},
+		{5, []int{1, 3, 5}, 2},
+		{0, []int{1, 3, 5}, -1},
+		{2, []int{1, 3, 5}, -1},
+		{4, []int{1, 3, 5}, -1},
+		{6, []int{1, 3, 5}, -1},
+
+		{1, []int{1, 3, 5, 7}, 0},
+		{3, []int{1, 3, 5, 7}, 1},
+		{5, []int{1, 3, 5, 7}, 2},
+		{7, []int{1, 3, 5, 7}, 3},
+		{0, []int{1, 3, 5, 7}, -1},
+		{2, []int{1, 3, 5, 7}, -1},
+		{4, []int{1, 3, 5, 7}, -1},
+		{6, []int{1, 3, 5, 7}, -1},
+		{8, []int{1, 3, 5, 7}, -1},
+	}
+
+	for _, test := range tests {
+		assert.Equal(test.Result, chop(test.Target, test.Array))
+	}
+}
